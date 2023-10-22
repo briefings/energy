f754f83c6250646eea06058b0945e3728a328971
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2023-10-22 14:32:59 +0100

    Add database/drop.sql

---
+++ database/drop.sql
@@ -0,0 +1,10 @@
+SHOW DATABASES;
+
+SHOW TABLES IN baseline;
+
+DROP TABLE IF EXISTS baseline.wesp;
+DROP TABLE IF EXISTS baseline.internal_geography;
+DROP TABLE IF EXISTS baseline.country;
+DROP DATABASE IF EXISTS baseline;
+
+SHOW DATABASES;
